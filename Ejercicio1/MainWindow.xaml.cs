5373b9e93ac6ed90aea3a6a2beb8ca8f43875df3
hp <hp@LAPTOP-RTAH6NE8>
2019-06-10 11:31:11 -0600

    Git add

--- Ejercicio1/MainWindow.xaml.cs
+++ Ejercicio1/MainWindow.xaml.cs
@@ -20,9 +20,23 @@ namespace Ejercicio1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Rectangulo Rec
+        {
+            get;set;
+        }
         public MainWindow()
         {
             InitializeComponent();
+            Rec = new Rectangulo
+            {
+                CoordenadaX1 = "1",
+                CoordenadaX2 = "2",
+                CoordenadaY1 = "3",
+                CoordenadaY2 = "4",
+                Resultado = "Resultado de coordenadas"
+
+            };
+            this.DataContext = Rec;
         }
     }
 }
